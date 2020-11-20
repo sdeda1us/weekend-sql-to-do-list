26f6af46fe7357bff853cfa6a7ae03e6b8209250
Steven Maloney <steven.maloney@gmail.com>
2020-11-19 20:22:15 -0600

    added AJAX for GET and POST in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,33 @@
-console.log('in javascript');
-
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('in jquery');
+    $('#submit-btn').on('click', storeTask);
+}
+
+
+//Sends entered task to the POST route server side
+function storeTask(event) {
+    event.preventDefault();
+    let taskObject = {
+        task: $('#task-in').val(),
+        complete: false
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/todos',
+        data: taskObject
+    })
+    .then(function(response) {
+        $('#task-in').val('');
+        getTodos();
+    })
+}
+
+//Activates GET route from server side
+function getTodos() {
+    $.ajax({
+        type: 'GET',
+        url: '/todos'
+      })
+      .then(console.log(resposne));
 }
