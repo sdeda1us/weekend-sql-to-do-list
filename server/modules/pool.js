d3c99831cf8a79e11bc3d95c72b7c527304bc6ef
Steven Maloney <steven.maloney@gmail.com>
2020-11-23 11:59:55 -0600

    changed pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,14 +3,26 @@ const pg = require('pg');
 // get the Pool object from pg
 const Pool= pg.Pool; 
 // Make our instance of a pool from that template pool object 
-const pool = new Pool({ 
-    // give pg some info on hwo to connect with our database 
-    database: 'weekend-to-do-app', //THIS WILL CHANGE -- your actual database name 
-    host: 'localhost', // connect to our local computer
-    port: 5432, // port
-    max: 10,    // max number of connection 
-    idleTimeoutMillis: 30000 // 30 sec
-}); 
+
+let config = {};
+
+if(process.env.DATABASE_URL){
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {rejectUnauthenticated: false}
+    }
+} else {
+    config = {
+        // give pg some info on hwo to connect with our database 
+        database: 'weekend-to-do-app', //THIS WILL CHANGE -- your actual database name 
+        host: 'localhost', // connect to our local computer
+        port: 5432, // port
+        max: 10,    // max number of connection 
+        idleTimeoutMillis: 30000 // 30 sec
+    }; 
+}
+
+const pool = new Pool(config);
 
 // When we connect to the database, fun function 
 pool.on('connect', () => {
